7b74c9bf236ff24b81731924a9c9e54e5e3cce94
Arjun <123224576+LearnCodeArjun@users.noreply.github.com>
2023-12-21 15:42:01 +0530

    changes some code for war

--- src/main/java/com/main/JSpConnectProjectApplication.java
+++ src/main/java/com/main/JSpConnectProjectApplication.java
@@ -2,12 +2,19 @@ package com.main;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class JSpConnectProjectApplication {
+public class JSpConnectProjectApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(JSpConnectProjectApplication.class, args);
+	}
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		// TODO Auto-generated method stub
+		return builder.sources(JSpConnectProjectApplication.class);
 	}
 
 }
